16e4a56563a9ca607ebc20913f1712a2be4f132f
anmaxwell <aniamaxwell@gmail.com>
2019-11-25 15:19:11 +0000

    added notes to tables and cleared out test tables

--- cvtracker/models.py
+++ cvtracker/models.py
@@ -4,6 +4,7 @@ from cvtracker import db
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60), nullable=False)
+    role_notes = db.Column(db.String(360))
     date_opened = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     mrg_id = db.Column(db.Integer, db.ForeignKey('hirer.id'), nullable=False)
     cvs = db.relationship('CV', backref="role", lazy=True)
@@ -15,6 +16,7 @@ class CV(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     surname = db.Column(db.String(60), nullable=False)
     status = db.Column(db.String(30), nullable=False)
+    cv_notes = db.Column(db.String(360))
     date_entered = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
     mrg_id = db.Column(db.Integer, db.ForeignKey('hirer.id'), nullable=False)
